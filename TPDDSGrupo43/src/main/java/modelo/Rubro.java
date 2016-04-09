a3fe3ce7fbdf994ddcc9f841848c043852f26c58
nicolaselopez <nicolas.e.lopez@gmail.com>
2016-04-09 13:30:00 -0300

    Se modifica llamada a dependencias por pom.xml y se resuelve error de Rubro

--- TPDDSGrupo43/src/main/java/modelo/Rubro.java
+++ TPDDSGrupo43/src/main/java/modelo/Rubro.java
@@ -8,43 +8,8 @@ import org.jasypt.util.password.BasicPasswordEncryptor;
 
 import controlador.BeanUsuario;
 
-public class Usuario {
+public class Rubro {
 	
-	public static BeanUsuario consultarUsuario(String usuario){
-		BeanUsuario busuario=null;
-		try{
-			Conexion c=new Conexion();
-			Connection con=c.getConexion();
-			Statement st=con.createStatement();
-			ResultSet rs=st.executeQuery("Select * from usuarios where usuario='"+usuario+"' and activo=1");
-			while(rs.next()){
-				busuario=new BeanUsuario(rs.getString(5), null, rs.getString(4), usuario, rs.getString(3));
-			}
-		}catch(SQLException se){
-			se.printStackTrace();
-			busuario.setUsuario(se.getLocalizedMessage());
-		}
-		return busuario;
-	}
 	
-	public static Boolean registrarUsuario(BeanUsuario usuario){
-		Boolean OK = false;
-		BasicPasswordEncryptor encriptador = new BasicPasswordEncryptor();
-		usuario.setClave(encriptador.encryptPassword(usuario.getClave()));
-		try{
-			Conexion c=new Conexion();
-			Connection con=c.getConexion();
-			Statement st=con.createStatement();
-			Integer rs=st.executeUpdate("INSERT INTO usuarios (usuario, password, email, nombre, fechanac,activo) VALUES "
-					+ "('"+usuario.getUsuario()+"','"+usuario.getClave()+"','"+usuario.getCorreo()+"','"+usuario.getNombre()+"',"+usuario.getEdad()+",1);");
-			if(rs == 1){
-				OK = true;
-			}
-		}catch(SQLException se){
-			se.printStackTrace();
-			OK=false;
-		}		
-		return OK;
-	}
 
 }
