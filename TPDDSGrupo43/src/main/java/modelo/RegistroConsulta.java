9cb88da7800adbcb445f973d959586e1dbe45326
Nicolas Ezequiel Lopez <nicolas.e.lopez@gmail.com>
2016-06-28 22:49:34 -0300

    Se comienza con registro de consultas

--- TPDDSGrupo43/src/main/java/modelo/RegistroConsulta.java
+++ TPDDSGrupo43/src/main/java/modelo/RegistroConsulta.java
@@ -1,10 +1,16 @@
 package modelo;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class RegistroConsulta {
 	
 	private int IdRegistroConsulta;
 	private int RegConIdUsuario;
 	private int RegConIdPoi;
+	private String RegConTipoConsulta;
+	private String RegConDetalle;
 	private String RegConFechaConsulta;
 	
 	public int getIdRegistroConsulta() {
@@ -31,19 +37,51 @@ public class RegistroConsulta {
 	public void setRegConFechaConsulta(String regConFechaConsulta) {
 		RegConFechaConsulta = regConFechaConsulta;
 	}
+	public int getRegConTipoConsulta() {
+		return RegConTipoConsulta;
+	}
+	public void setRegConTipoConsulta(String regConTipoConsulta) {
+		RegConTipoConsulta = regConTipoConsulta;
+	}
+	public String getRegConDetalle() {
+		return RegConDetalle;
+	}
+	public void setRegConDetalle(String regConDetalle) {
+		RegConDetalle = regConDetalle;
+	}
 	
-	public RegistroConsulta(int idRegistroConsulta, int regConIdUsuario, int regConIdPoi,
-			String regConFechaConsulta) {
+	public RegistroConsulta(int idRegistroConsulta, int regConIdUsuario, int regConIdPoi, String regConTipoConsulta,
+			String regConDetalle, String regConFechaConsulta) {
 		super();
 		IdRegistroConsulta = idRegistroConsulta;
 		RegConIdUsuario = regConIdUsuario;
 		RegConIdPoi = regConIdPoi;
+		RegConTipoConsulta = regConTipoConsulta;
+		RegConDetalle = regConDetalle;
 		RegConFechaConsulta = regConFechaConsulta;
 	}
-	
 	public RegistroConsulta() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+	
+	public Boolean registrarConsulta(RegistroConsulta consulta){
+		Boolean OK =false;
+		try{
+			Conexion c=new Conexion();
+			Connection con=c.getConexion();
+			Statement st=con.createStatement();
+			Integer rs = st.executeUpdate("INSERT INTO registroconsulta(RegConIdUsuario,RegConIdPoi,RegConTipoConsulta,RegConDetalle) VALUES("
+					+consulta.RegConIdUsuario+","+consulta.RegConIdPoi+",'"+consulta.RegConTipoConsulta+"','"+consulta.RegConDetalle+"');");
+		if(rs == 1){
+			OK=true;
+		}
+		}catch(SQLException se){
+			se.printStackTrace();
+			OK=false;
+		}
+		return OK;
+	}
+	
 	
 }
