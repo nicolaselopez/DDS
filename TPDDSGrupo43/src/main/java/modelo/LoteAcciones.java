e8b087695bedb36f9cdba13c42cc10922139cfe4
Nicolas Ezequiel Lopez <nicolas.e.lopez@gmail.com>
2016-08-26 16:12:35 -0300

    terminado proceso 3

--- TPDDSGrupo43/src/main/java/modelo/LoteAcciones.java
+++ TPDDSGrupo43/src/main/java/modelo/LoteAcciones.java
@@ -1,5 +1,10 @@
 package modelo;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+
 public class LoteAcciones {
 
 	private boolean accion1 = false;
@@ -89,6 +94,39 @@ public class LoteAcciones {
 	public LoteAcciones() {
 		super();
 		// TODO Auto-generated constructor stub
+	}
+	
+	public boolean registrarAcciones(int usuario, int secuencial) {
+		// TODO Auto-generated method stub
+		boolean OK = true;
+		OK = (this.accion1) ? registrarAccion(usuario,secuencial,1) : true;
+		OK = (this.accion2) ? registrarAccion(usuario,secuencial,2) : true;
+		OK = (this.accion3) ? registrarAccion(usuario,secuencial,3) : true;
+		OK = (this.accion4) ? registrarAccion(usuario,secuencial,4) : true;
+		OK = (this.accion5) ? registrarAccion(usuario,secuencial,5) : true;
+		OK = (this.accion6) ? registrarAccion(usuario,secuencial,6) : true;
+		OK = (this.accion7) ? registrarAccion(usuario,secuencial,7) : true;
+		OK = (this.accion8) ? registrarAccion(usuario,secuencial,8) : true;
+		OK = (this.accion9) ? registrarAccion(usuario,secuencial,9) : true;
+		OK = (this.accion10) ? registrarAccion(usuario,secuencial,10) : true;
+		OK = (this.accion11) ? registrarAccion(usuario,secuencial,11) : true;
+		OK = (this.accion12) ? registrarAccion(usuario,secuencial,12) : true;
+		return OK;
+	}
+	
+	private boolean registrarAccion(int usuario, int secuencial, int accion){
+		boolean OK = true;
+		try{
+			Conexion c=new Conexion();
+			Connection con=c.getConexion();
+			Statement st=con.createStatement();
+			Integer rs=st.executeUpdate("INSERT INTO acciones_x_usuario (Usuario,Accion,"
+					+ "FechaDeCreacion,Activo,Secuencial) VALUES ("+usuario+","
+					+accion+",'"+LocalDate.now().toString()+"',"+1+","+secuencial+");");		
+		}catch(SQLException se){
+			se.printStackTrace();
+		}
+		return OK;
 	}
 	
 }
--- TPDDSGrupo43/src/main/java/modelo/Proceso3.java
+++ TPDDSGrupo43/src/main/java/modelo/Proceso3.java
@@ -1,5 +1,10 @@
 package modelo;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 //import java.sql.Connection;
 //import java.sql.ResultSet;
 //import java.sql.Statement;
@@ -8,11 +13,86 @@ package modelo;
 public class Proceso3 extends ProcesoStr {
 
 	@Override
-	public void procesar(String usuario, LoteAcciones acciones) {
-		
+	public void procesar(String usu, LoteAcciones acciones) {
+		boolean OK = false;
+		Usuario usuario = Usuario.consultarUsuario(usu);
+		int secuencial = buscarAccionDeUsuario(usuario.getIdUsuario());
+		if(secuencial != 0){
+			OK = modificarSecuencial(usuario.getIdUsuario(),secuencial,0); 
+		}
+		OK = darAltaAcciones(usuario.getIdUsuario(),acciones,secuencial+1);
 		Accion_x_Usuario AxU = new Accion_x_Usuario(); // Instanciar vacio
 		UsuarioAdministrador admin = new UsuarioAdministrador();
 		AxU.iniciarProcesoDe(admin);
+	}
+	
+	private static int buscarAccionDeUsuario(int usuario){
+		int secuencial = 0;
+		try{
+			Conexion c=new Conexion();
+			Connection con=c.getConexion();
+			Statement st=con.createStatement();
+			ResultSet rs=st.executeQuery("Select MAX(Secuencial) from acciones_x_usuario where Usuario="+ usuario +" and Activo=1");
+			while(rs.next()){
+				secuencial = rs.getInt(1);
+			}
+		}catch(Exception se){
+			se.printStackTrace();
+		}
+		return secuencial;
+	}
+	
+	private static boolean darAltaAcciones(int usuario, LoteAcciones acciones, int secuencial){
+		boolean OK = false;
+		OK = acciones.registrarAcciones(usuario,secuencial);
+		return OK;
+	}
+	
+	private static boolean modificarSecuencial(int usuario, int secuencial, int activo){
+		Boolean OK =false;
+		try{
+			Conexion c=new Conexion();
+			Connection con=c.getConexion();
+			Statement st=con.createStatement();
+			Integer rs = st.executeUpdate("UPDATE acciones_x_usuario SET Activo = "+activo+" where Usuario = " + usuario + " and Secuencial = "+ secuencial +";");
+		if(rs==1){
+			OK=true;
+		}
+		}catch(SQLException se){
+			se.printStackTrace();
+			OK=false;
+		}
+		return OK;
+	}
+	
+	public boolean rollback(String usu){
+		boolean OK = false;
+		Usuario usuario = Usuario.consultarUsuario(usu);
+		int secuencial = buscarAccionDeUsuario(usuario.getIdUsuario());
+		if(secuencial != 0){
+			OK = borrarSecuencial(usuario.getIdUsuario(),secuencial);
+			if(secuencial > 1){
+				OK = modificarSecuencial(usuario.getIdUsuario(),secuencial-1,1); 
+			}
+		}
+		return OK;
+	}
+	
+	private static boolean borrarSecuencial(int usuario, int secuencial){
+		Boolean OK =false;
+		try{
+			Conexion c=new Conexion();
+			Connection con=c.getConexion();
+			Statement st=con.createStatement();
+			Integer rs = st.executeUpdate("DELETE FROM acciones_x_usuario where Usuario = " + usuario + " and Secuencial = "+ secuencial +";");
+		if(rs==1){
+			OK=true;
+		}
+		}catch(SQLException se){
+			se.printStackTrace();
+			OK=false;
+		}
+		return OK;
 	}
 
 }	
